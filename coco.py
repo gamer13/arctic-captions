2a7f6ac110f2e52ff5f5dfca105f427c3e9149ab
Flip van Rijn <flipvrijn@gmail.com>
2016-01-21 14:41:39 +0100

    Added path argument

--- coco.py
+++ coco.py
@@ -53,7 +53,7 @@ def prepare_data(caps, features, worddict, maxlen=None, n_words=10000, zero_pad=
 
     return x, x_mask, y
 
-def load_data(load_train=True, load_dev=True, load_test=True, path='/media/Data/flipvanrijn/datasets/coco/processed/full/'):
+def load_data(load_train=True, load_dev=True, load_test=True, path='./'):
     ''' Loads the dataset
 
     :type dataset: string
@@ -67,26 +67,26 @@ def load_data(load_train=True, load_dev=True, load_test=True, path='/media/Data/
     print '... loading data'
 
     if load_train:
-        with open(path+'coco_align.train.pkl', 'rb') as f:
+        with open(path+'/coco_align.train.pkl', 'rb') as f:
             train_cap = pkl.load(f)
             train_feat = pkl.load(f)
         train = (train_cap, train_feat)
     else:
         train = None
     if load_test:
-        with open(path+'coco_align.test.pkl', 'rb') as f:
+        with open(path+'/coco_align.test.pkl', 'rb') as f:
             test_cap = pkl.load(f)
             test_feat = pkl.load(f)
         test = (test_cap, test_feat)
     else:
         test = None
     if load_dev:
-        with open(path+'coco_align.dev.pkl', 'rb') as f:
+        with open(path+'/coco_align.dev.pkl', 'rb') as f:
             dev_cap = pkl.load(f)
             dev_feat = pkl.load(f)
         valid = (dev_cap, dev_feat)
 
-    with open(path+'dictionary.pkl', 'rb') as f:
+    with open(path+'/dictionary.pkl', 'rb') as f:
         worddict = pkl.load(f)
 
     return train, valid, test, worddict
